9f2ceebf661bfeeaf608750b1f9824f07fedc52c
Fahim Al-Noor <fahimalnoor1@gmail.com>
2024-05-13 20:36:47 -0230

    get all employees

--- ems-backend/src/main/java/com/softonech/ems/service/EmployeeService.java
+++ ems-backend/src/main/java/com/softonech/ems/service/EmployeeService.java
@@ -2,8 +2,12 @@ package com.softonech.ems.service;
 
 import com.softonech.ems.dto.EmployeeDto;
 
+import java.util.List;
+
 public interface EmployeeService {
     EmployeeDto createEmployee(EmployeeDto employeeDto);
 
     EmployeeDto getEmployeeById(Long employeeId);
+
+    List<EmployeeDto> getAllEmployees();
 }
